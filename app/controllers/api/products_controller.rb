40bd26be7a8311e60e9ecbeb2595ffbf33323c2e
Ken Tran <loschorts@gmail.com>
2017-01-29 23:22:31 -0800

    rename ensure_timeframe_params

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,5 @@
 class Api::ProductsController < ApplicationController
-	before_action :ensure_interval_params, only: [:sales]
+	before_action :ensure_timeframe_params, only: [:sales]
 
 	def sales
 		@sales = Product.sales_in_timeframe(timeframe_params).as_json
@@ -12,10 +12,9 @@ class Api::ProductsController < ApplicationController
 		params.permit(:start_date, :end_date, :interval, :group_by, product_ids: [])
 	end
 	
-	def ensure_interval_params
+	def ensure_timeframe_params
 		intervals = %w(day week month)
 		params[:interval] = intervals.detect {|i| i == params[:interval]} || "month"
-
 		params[:start_date] ||= "1-1-1"
 		params[:end_date] ||= "9-9-9999"
 	end
